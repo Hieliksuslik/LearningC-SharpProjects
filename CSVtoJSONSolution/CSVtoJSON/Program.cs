54254326058806e7a5dc0c56ae254a2f2ced6aa9
Landon Johnson <80288546+Hieliksuslik@users.noreply.github.com>
2023-03-13 09:46:20 -0500

    Added CSVtoJSON project

---
+++ CSVtoJSONSolution/CSVtoJSON/Program.cs
@@ -0,0 +1,43 @@
+﻿using CSVtoJSONlib;
+using System;
+using System.IO;
+using System.Reflection;
+
+// This is my current solution to the C# Foundations 1
+// project By: Matthew Hess
+// Landon Johnson
+// CGInfinity
+// 03/13/2023
+
+namespace CSVtoJSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the CSV - to - JSON Utility!");
+            Console.WriteLine("Please enter the path to the CSV file: ");
+
+            string filePath = Console.ReadLine();
+
+            if(!File.Exists(filePath))
+            {
+                // Console.WriteLine("File does not exist/invalid path!");
+                throw new Exception("File Does not Exist/Invalid Path.");
+            }
+            int validAccounts = 0;
+            int validTransactions = 0;
+            try 
+            {
+                Processor.ProcessLines(filePath, out validAccounts, out validTransactions);
+                Console.WriteLine("Success!");
+                Console.WriteLine($"Total number of Valid Accounts: {validAccounts}. \nTotal number of Valid Transactions: {validTransactions}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Process line failed, Exception: {ex.ToString()}");
+            }
+        }
+    }
+}
---
+++ CSVtoJSONSolution/CSVtoJSONlib/Processor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace CSVtoJSONlib
+{
+    internal class Data
+    {
+        public int TransactionID { get; set; }
+        public DateOnly TransactionDate { get; set; }
+        public decimal TransactionAmount { get; set; }
+        public string? AccountID { get; set; }
+        public string? AccountOwnerName { get; set; }
+        public string? AccountType { get; set; }
+    }
+
+    public class Processor
+    {
+        public static void ProcessLines(string path, out int validAccounts, out int validTransactions)
+        {
+            List<Data> _data = new List<Data>();
+            validAccounts = 0;
+            validTransactions = 0;
+
+            // Get filename without extension
+            string fileNameNoExtension = System.IO.Path.ChangeExtension(path, null);
+
+            using (TextFieldParser parser = new TextFieldParser(path))
+            {
+                parser.SetDelimiters(",");
+                string[] currentRow = parser.ReadFields();
+                while (!parser.EndOfData)
+                {
+                    currentRow = parser.ReadFields();
+                    if(Array.IndexOf(currentRow, string.Empty) == -1)
+                    {
+                        _data.Add(new Data() 
+                        {
+                            TransactionID = int.Parse(currentRow[0]),
+                            TransactionDate = DateOnly.Parse(currentRow[1]),
+                            TransactionAmount = decimal.Parse(currentRow[2]),
+                            AccountID = currentRow[3],
+                            AccountOwnerName = currentRow[4],
+                            AccountType = currentRow[5]
+                        });
+
+                        // TODO: Determine how to properly count unique account ID's.
+                        validAccounts++;
+                        validTransactions++;
+                    }
+                }
+            }
+
+            string json = JsonConvert.SerializeObject( _data.ToArray(), Formatting.Indented);
+            System.IO.File.WriteAllText(fileNameNoExtension + ".json", json);
+        }
+    }
+}
