3a44e3e7752643e15fe44ad48b6bc6bc548341fe
Landon Johnson <hieliksuslik@gmail.com>
2023-03-25 01:42:12 -0500

    Using TTT, tested field creation/print

--- TictacToe/TicTacConsole/Program.cs
+++ TictacToe/TicTacConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using TictacToe;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -7,6 +7,16 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Game game = new Game();
+            char[,] field = new char[3,3] {
+                {'.','.','.'},
+                {'.','.','.'},
+                {'.','.','.'}
+            };
+
+            game.Field = field;
+
+            game.PrintField();
         }
     }
 }
