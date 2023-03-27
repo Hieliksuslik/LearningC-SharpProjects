521fdd29dc463d11f14178201dc77d6d9f9b2781
Landon Johnson <hieliksuslik@gmail.com>
2023-03-27 11:37:38 -0500

    Updated PrintField(), Created PlaceMarker()

--- TictacToe/TicTacConsole/Game.cs
+++ TictacToe/TicTacConsole/Game.cs
@@ -15,18 +15,40 @@ namespace TictacToe
                 _field = value;
             }
         }
-    public void PrintField()
-    {
-        for (int i = 0; i < 3; i++)
+        public void PrintField()
         {
-            for (int j = 0; j < 3; j++)
-            {
-                Console.Write(_field[i,j]);
-            }
             Console.WriteLine();
+            Console.WriteLine(" 012");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(i);
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(_field[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // TODO: Landon, when you wake up re-think how to approach 
+        // using this.
+        public bool PlaceMarker(int x, int y, char player)
+        {
+            // If out of bounds, invalid move
+            if( x < 0 || x > 2 || y < 0 || y > 2) 
+                return false;
+            // If not a free space, invalid move
+            if(_field[x,y] != '.')
+                return false;
+             
+            _field[x,y] = player;
+            return true;
+        }
+
+        public void CheckWin()
+        {
+
         }
     }
-    }
 
-    
 }
