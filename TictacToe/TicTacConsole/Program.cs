854f69720943446948dc53656446b9cece99c61a
Landon Johnson <hieliksuslik@gmail.com>
2023-03-27 11:37:59 -0500

    Introduced game loop

--- TictacToe/TicTacConsole/Program.cs
+++ TictacToe/TicTacConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using TictacToe;
+using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -7,16 +8,38 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Game game = new Game();
+            char player = 'X';
+            int x, y;
+
+            bool gameOver = false;
+
             char[,] field = new char[3,3] {
                 {'.','.','.'},
                 {'.','.','.'},
                 {'.','.','.'}
             };
 
+            Game game = new Game();
             game.Field = field;
 
             game.PrintField();
+
+            do
+            {
+                Console.WriteLine("Type the x-coordinate of your mark: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Type the y-coordinate of your mark: ");
+                y = Convert.ToInt32(Console.ReadLine());
+
+                // check if valid move
+                if(!game.PlaceMarker(x, y, player)){
+                    Console.WriteLine("Invalid move, please enter a valid x, y.");
+                    continue;
+                }
+
+                game.PrintField();
+
+            } while (!gameOver);
         }
     }
 }
